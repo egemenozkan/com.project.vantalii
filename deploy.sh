eb668aba8db9b2b4800c8144b2fa069bb43cfe9c
egemenozkan@gmail.com <egemenozkan@gmail.com>
2021-03-03 08:27:57 +0300

    fixed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#mvn -f $HOME/devel/workspace/projects/com.project.contract/ clean install
+mvn -f $HOME/Devel/workspace/projects/com.project.contract/ clean install
 mvn clean package -DskipTests -Pprod
 
 #StaticFiles
-rsync -acrvz  --delete --stats --progress target/classes/static/ devops@94.237.94.131:/opt/www/vantalii/static
+rsync -acrvz  --delete --stats --progress target/web-alfa/WEB-INF/classes/static/ webuser@192.168.0.20:/var/depo/vantalii/static
 
 #rsync -Cavuzb -a --stats --progress target/web/ devops@dc.sevais.com:/home/devops/www/sevais/opetra/core/webapps/app
-rsync -acrvz  --delete --stats --progress target/web-alfa/ devops@94.237.94.131:/var/www/jee_projects/vantalii/apache-tomcat-9.0.34/webapps/ROOT
+rsync -acrvz  --delete --stats --progress target/web-alfa/ webuser@192.168.0.20:/var/www/vantalii/web/webapps/ROOT
 exit 1;
 
